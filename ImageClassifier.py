77c452df08ea55cab9d540c87a504f5013cb37e0
Luka <luka.kordic@cobeisfresh.com>
2019-04-11 21:10:03 +0200

    Create fashion MNIST classifier

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -1,66 +1,104 @@
 from __future__ import absolute_import, division, print_function
 
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
-import tensorflow_datasets as tfds
-
-tf.logging.set_verbosity(tf.logging.ERROR)
-
-# Helper libraries
-import math
-
-# Improve progress bar display
-import tqdm.auto
-
-tqdm.tqdm = tqdm.auto.tqdm
+from tensorflow import keras
 
 print(tf.__version__)
 
-tf.enable_eager_execution()
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-# Load dataset, split into train and test sets
-dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
-train_dataset, test_dataset = dataset['train'], dataset['test']
+print(train_labels)
+class_names = ['tshirt/top', 'trouser', 'pullover', 'dress', 'coat',
+               'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# this line prints out (60000, 28, 28) which means there are 60000 images of size 28x28 pixels
+print(train_images.shape)
 
-num_train_examples = metadata.splits['train'].num_examples
-num_test_examples = metadata.splits['test'].num_examples
-print("Number of training examples: {}".format(num_train_examples))
-print("Number of test examples:     {}".format(num_test_examples))
+# this line prints out (10000, 28, 28) which means there are 10000 images of size 28x28 pixels
+print(test_images.shape)
 
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
-# The value of each pixel in the image data is an integer in the range [0,255].
-# For the model to work properly, these values need to be normalized to the range [0,1].
-# So here we create a normalization function, and then apply it to each image in the test and train datasets.
-def normalize(images, labels):
-    images = tf.cast(images, tf.float32)
-    images /= 255
-    return images, labels
+# IMAGE PREPROCESSING
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i + 1)
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel([class_names[train_labels[i]]])
+# plt.show()
 
-train_dataset = train_dataset.map(normalize)
-test_dataset = test_dataset.map(normalize)
-
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation=tf.nn.relu, input_shape=(28, 28, 1)),
-    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
-    tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation=tf.nn.relu),
-    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, tf.nn.relu),
-    tf.keras.layers.Dense(10, tf.nn.softmax)
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # transform input to 1D vector (28*28=784)
+    keras.layers.Dense(128, activation=tf.nn.relu),  # Dense = fully connected
+    keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-BATCH_SIZE = 32
-train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
-test_dataset = test_dataset.batch(BATCH_SIZE)
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print('test accuracy:', test_accuracy)
+print('test loss:', test_loss)
+
+predictions = model.predict(test_images)
 
 
-model.fit(train_dataset, epochs=10, steps_per_epoch=math.ceil(num_train_examples / BATCH_SIZE))
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
 
 
-test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples/32))
-print('Accuracy on test dataset')
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+num_rows = 5
+num_cols = 3
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+    plot_value_array(i, predictions, test_labels)
+plt.show()
